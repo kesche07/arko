8658702d3243199de4b77cc058ac62427619512c
keschella parajuli <keschellap@gmail.com>
2025-11-19 12:58:00 +0545

    statement updates

--- newclass.java
+++ newclass.java
@@ -1,8 +1,18 @@
 
 public class newclass
 {
+    int age;// instance variable
+    static int agr; //static variable
     public static void main(String[] args){
     System.out.println("hello");
+    int qty; //local variable
+    qty=2;
+    //<classname> <variable>=new <className>();
+    newclass gr=new newclass();
+    
+    System.out.println(gr.age);
+    System.out.println(agr);
+    System.out.println(qty);
     
     }
 }
